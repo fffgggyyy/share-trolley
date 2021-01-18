5d8fa737349db0a651e4f7bc96b4d957cf3518d0
fsk <1694385261@qq.com>
2021-01-18 15:43:14 +0800

    the thid

--- src/main/java/com/nju/server/admin/fskgittest/fsk2.java
+++ src/main/java/com/nju/server/admin/fskgittest/fsk2.java
@@ -1,4 +1,7 @@
 package com.nju.server.admin.fskgittest;
 
 public class fsk2 {
+    public static void main(String[] args) {
+        System.out.println("hello fsk");
+    }
 }
--- src/main/java/com/nju/server/admin/fskgittest/fsk3.java
+++ src/main/java/com/nju/server/admin/fskgittest/fsk3.java
@@ -1,4 +1,7 @@
 package com.nju.server.admin.fskgittest;
 
 public class fsk3 {
+    public static void main(String[] args) {
+        System.out.println("fsk the third commit");
+    }
 }
